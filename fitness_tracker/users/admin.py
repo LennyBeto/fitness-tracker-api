a415b9c6c95a95cb587610d8a432032d5868cfe0
Lenny Beto <lennybeto95@gmail.com>
2026-02-17 21:52:05 +0300

    feat: admin configured for users app

--- fitness_tracker/users/admin.py
+++ fitness_tracker/users/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    """
+    Inline admin for UserProfile
+    """
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fields = (
+        'date_of_birth', 'gender', 'height', 'weight',
+        'bio', 'profile_picture'
+    )
+
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Extended User admin with profile
+    """
+    inlines = (UserProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'date_joined')
+
+
+# Unregister the original User admin and register the new one
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    Admin for UserProfile model
+    """
+    list_display = ('user', 'gender', 'age', 'height', 'weight', 'bmi', 'created_at')
+    list_filter = ('gender', 'created_at')
+    search_fields = ('user__username', 'user__email', 'user__first_name', 'user__last_name')
+    readonly_fields = ('created_at', 'updated_at', 'age', 'bmi')
+    
+    fieldsets = (
+        ('User', {
+            'fields': ('user',)
+        }),
+        ('Personal Information', {
+            'fields': ('date_of_birth', 'gender', 'bio', 'profile_picture')
+        }),
+        ('Physical Stats', {
+            'fields': ('height', 'weight', 'age', 'bmi')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
